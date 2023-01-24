06d1875cb7ef7e63f0f56a74f57a866de01e028f
jwelyl <cork2586@naver.com>
2023-01-24 14:46:42 +0900

    230124 14:46 UserJpaRepo 구현 Spring Data JPA 사용하여 JpaRepository 상속해서 사용함. 일반적으로 통용되는 DAO에 대한 CRUD 메서드 구현됨.

---
+++ src/main/java/com/a504/userdemo/repository/UserJpaRepo.java
@@ -0,0 +1,9 @@
+package com.a504.userdemo.repository;
+
+import com.a504.userdemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepo extends JpaRepository<User, Long> {
+    User findByName(String name);
+    User findByEmail(String email);
+}
