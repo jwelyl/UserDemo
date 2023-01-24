b36ee09d0e9a5a41b6d6e34b7ffb16341f69533b
jwelyl <cork2586@naver.com>
2023-01-24 17:56:06 +0900

    230124 17:56 DTO 구현 Entity와 DTO를 구별하여 DTO를 API 요청, 응답값으로 사용

---
+++ src/main/java/com/a504/userdemo/dto/UserRequestDto.java
@@ -0,0 +1,29 @@
+package com.a504.userdemo.dto;
+
+import com.a504.userdemo.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRequestDto {
+    private String email;
+    private String name;
+
+    @Builder
+    public UserRequestDto(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .email(email)
+                .name(name)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/a504/userdemo/dto/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.a504.userdemo.dto;
+
+import com.a504.userdemo.entity.User;
+import lombok.Getter;
+
+@Getter
+public class UserResponseDto {
+    private final Long userId;
+    private final String email;
+    private final String name;
+
+    public UserResponseDto(User user) {
+        this.userId = user.getUserId();
+        this.email = user.getEmail();
+        this.name = user.getName();
+    }
+}
