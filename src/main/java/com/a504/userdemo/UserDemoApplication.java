0d847375e66b66defff61cefaee9963a787afc17
jwelyl <cork2586@naver.com>
2023-01-24 11:21:40 +0900

    Initial commit

---
+++ src/main/java/com/a504/userdemo/UserDemoApplication.java
@@ -0,0 +1,13 @@
+package com.a504.userdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a504/userdemo/UserDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a504.userdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
