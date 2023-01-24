90d185841de4c626d4b9e2093378c515845bb6ff
jwelyl <cork2586@naver.com>
2023-01-24 15:53:34 +0900

    230124 15:53 다중 응답 모델 API 반환값이 다중 객체일 경우

---
+++ src/main/java/com/a504/userdemo/model/response/ListResult.java
@@ -0,0 +1,16 @@
+package com.a504.userdemo.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ *  다중 응답 모델
+ *  API 반환값이 다중 객체일 경우 해당 모델로 처리
+ */
+@Getter
+@Setter
+public class ListResult<T> {
+    private List<T> data;
+}
